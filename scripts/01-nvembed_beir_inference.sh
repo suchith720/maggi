839dd54b371b7e049c0dc2541e62a5dedbed17e4
t-utiwari <t-utiwari@microsoft.com>
2026-06-01 03:13:54 -0700

    NV-Embed-v2 BEIR Inference

--- scripts/01-nvembed_beir_inference.sh
+++ scripts/01-nvembed_beir_inference.sh
@@ -1,4 +1,8 @@
-datasets="arguana climate-fever dbpedia-entity fever fiqa hotpotqa nfcorpus nq quora scidocs scifact webis-touche2020 trec-covid \
+# datasets="arguana climate-fever dbpedia-entity fever fiqa hotpotqa nfcorpus nq quora scidocs scifact webis-touche2020 trec-covid \
+# 	cqadupstack/android cqadupstack/english cqadupstack/gaming cqadupstack/gis cqadupstack/mathematica cqadupstack/physics cqadupstack/programmers \
+# 	cqadupstack/stats cqadupstack/tex cqadupstack/unix cqadupstack/webmasters cqadupstack/wordpress"
+
+datasets="nfcorpus nq quora scidocs scifact webis-touche2020 trec-covid \
 	cqadupstack/android cqadupstack/english cqadupstack/gaming cqadupstack/gis cqadupstack/mathematica cqadupstack/physics cqadupstack/programmers \
 	cqadupstack/stats cqadupstack/tex cqadupstack/unix cqadupstack/webmasters cqadupstack/wordpress"
 
